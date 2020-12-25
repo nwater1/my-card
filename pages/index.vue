2c7f63d209a11eb3efc34c61d6be0d2c92b6b231
Mint <mint167349@gmail.com>
2020-12-25 22:13:04 +0700

    change image by device

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,9 @@
 <template>
   <v-row justify="center" align="center">
-    <v-main style="padding-bottom: 50px">
+    <v-main style="padding-bottom: 60px">
       <section id="main">
-        <v-img
-          max-height="1500"
-          width="100%"
-          src="https://new-year-card-1.s3-ap-southeast-1.amazonaws.com/banner2.jpg"
-        ></v-img>
-        <v-row justify="center" align="center" style="height: 100px">
+        <div class="img"></div>
+        <v-row justify="center" align="center" style="height: 110px">
           <a class="btn-1" href="https://lin.ee/Jqdw87d"
             >เพิ่มเพื่อน เพื่อรับการแจ้งเตือนสวดมนต์</a
           >
@@ -30,13 +26,13 @@ export default {
 <style scoped>
 .btn {
   color: white;
-  font-size: 22px;
-  padding: 12px 24px;
+  font-size: 20px;
+  padding: 8px 24px;
   border: none;
   cursor: pointer;
 
   text-align: center;
-  margin-top: 25px;
+  margin-top: 30px;
   width: 100%;
   height: 100%;
   background-color: #00c300;
@@ -53,9 +49,26 @@ a:link {
   cursor: pointer;
   display: block;
   text-align: center;
-  margin-top: 25px;
+  margin-top: 30px;
   width: 60%;
   height: 100%;
   background-color: #00c300;
+}
+div.img {
+  width: 100%;
+  height: 400px;
+  background-image: url('https://new-year-card-1.s3-ap-southeast-1.amazonaws.com/bannup.jpg');
+  background-size: cover;
+  border: 1px solid black;
+}
+
+@media only screen and (min-width: 960px) {
+  div.img {
+    background-image: url('https://new-year-card-1.s3-ap-southeast-1.amazonaws.com/banner2.jpg');
+    width: 100%;
+    height: 400px;
+    background-size: cover;
+    border: 1px solid black;
+  }
 }
 </style>
